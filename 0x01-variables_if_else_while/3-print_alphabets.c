75ee322615f44518913fc661b93d7a3f509b566c
mitchellshera <mitchellwachera@gmail.com>
2023-02-17 06:09:02 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char w = 'a', u = 'A';
+
+	for (; x <= 'z'; x++)
+		putchar(w);
+	for (; y <= 'Z'; y++)
+		putchar(u);
+	putchar('\n');
+	return (0);
+}
