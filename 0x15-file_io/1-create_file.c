1e476aa2d29fc6fe96f0bdb089fbc9cb135a41a9
mitchellshera <mitchellwachera@gmail.com>
2023-04-10 23:14:57 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
+	int fd, j;
 
 	if (!filename)
 		return (-1);
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
+		j = write(fd, text_content, strlen(text_content));
+		if (j == -1)
 		{
 			close(fd);
 			return (-1);
