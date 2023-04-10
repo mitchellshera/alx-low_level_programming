86332f334a40f2f5b654742e0d57b2b3dd921286
mitchellshera <mitchellwachera@gmail.com>
2023-04-10 23:13:50 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
+
 /**
- * create_file - Creates a file with the given name and text content
- * @filename: Name of the file to create
- * @text_content: NULL-terminated string to write to the file
+ * create_file - creates a file with the given filename and text content
+ * @filename: the name of the file to create
+ * @text_content: a NULL-terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
+	int fd, bytes_written;
+
+	if (!filename)
 		return (-1);
 
-	/* Open file with filename in write mode, truncating if it already exists */
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	/* Write text_content to file, if not NULL */
-	if (text_content != NULL)
+	if (text_content)
 	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-
+		bytes_written = write(fd, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
 			close(fd);
@@ -30,14 +29,6 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	/* Set file permissions to rw------- */
-	if (fchmod(fd, S_IRUSR | S_IWUSR) == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	/* Close file and return 1 on success */
 	close(fd);
 	return (1);
 }
