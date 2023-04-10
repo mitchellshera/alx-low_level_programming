f8adb36f00c6ad124d60625900e75f35efadb31e
mitchellshera <mitchellwachera@gmail.com>
2023-04-10 18:09:15 +0300

    5

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,13 +7,13 @@
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsigned long int current;
+	unsigned long int curr;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
+		curr = n >> i;
 
-		if (current & 1)
+		if (curr & 1)
 		{
 			_putchar('1');
 			count++;
@@ -21,6 +21,4 @@ void print_binary(unsigned long int n)
 		else if (count)
 			_putchar('0');
 	}
-	if (!count)
-		_putchar('0');
-}
+	
