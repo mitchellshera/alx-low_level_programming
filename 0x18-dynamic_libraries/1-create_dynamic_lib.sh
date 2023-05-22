5f9dccf343a92018ddf605453060998468b6e5fc
mitchellshera <64257596+mitchellshera@users.noreply.github.com>
2023-05-22 20:43:23 +0300

    1

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+c_files=$(ls *.c 2>/dev/null)
+
+if [ -z "$c_files" ]; then
+  echo "No .c files found in the current directory."
+  exit 1
+fi
+
+gcc -shared -o liball.so $c_files
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+  echo "Dynamic library liball.so created successfully."
+else
+  echo "Failed to create the dynamic library."
+fi
+1
