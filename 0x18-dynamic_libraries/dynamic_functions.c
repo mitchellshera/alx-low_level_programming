04c94f502a18efc693db71fe3c0db0988687535d
mitchellshera <64257596+mitchellshera@users.noreply.github.com>
2023-05-22 20:18:39 +0300

    fynamin

--- 0x18-dynamic_libraries/dynamic_functions.c
+++ 0x18-dynamic_libraries/dynamic_functions.c
@@ -1,127 +1,107 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int _putchar(char c)
-{
+int _putchar(char c) {
     return putchar(c);
 }
 
-int _islower(int c)
-{
+int _islower(int c) {
     return (c >= 'a' && c <= 'z');
 }
 
-int _isalpha(int c)
-{
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+int _isalpha(int c) {
+    return (_islower(c) || _isupper(c));
 }
 
-int _abs(int n)
-{
-    return (n < 0 ? -n : n);
+int _abs(int n) {
+    return (n >= 0 ? n : -n);
 }
 
-int _isupper(int c)
-{
+int _isupper(int c) {
     return (c >= 'A' && c <= 'Z');
 }
 
-int _isdigit(int c)
-{
+int _isdigit(int c) {
     return (c >= '0' && c <= '9');
 }
 
-int _strlen(char *s)
-{
-    int len = 0;
-    while (*s)
-    {
-        len++;
+int _strlen(char *s) {
+    int length = 0;
+    while (*s != '\0') {
+        length++;
         s++;
     }
-    return len;
+    return length;
 }
 
-void _puts(char *s)
-{
-    while (*s)
-    {
+void _puts(char *s) {
+    while (*s != '\0') {
         _putchar(*s);
         s++;
     }
     _putchar('\n');
 }
 
-char *_strcpy(char *dest, char *src)
-{
-    char *start = dest;
-    while (*src)
-    {
+char *_strcpy(char *dest, char *src) {
+    char *original_dest = dest;
+    while (*src != '\0') {
         *dest = *src;
         dest++;
         src++;
     }
     *dest = '\0';
-    return start;
+    return original_dest;
 }
 
-int _atoi(char *s)
-{
+int _atoi(char *s) {
     int sign = 1;
     int result = 0;
 
-    if (*s == '-')
-    {
+    if (*s == '-') {
         sign = -1;
         s++;
     }
 
-    while (*s)
-    {
-        if (*s >= '0' && *s <= '9')
-        {
+    while (*s != '\0') {
+        if (*s >= '0' && *s <= '9') {
             result = result * 10 + (*s - '0');
-            s++;
-        }
-        else
-        {
+        } else {
             break;
         }
+        s++;
     }
 
-    return result * sign;
+    return sign * result;
 }
 
-char *_strcat(char *dest, char *src)
-{
-    char *start = dest;
+char *_strcat(char *dest, char *src) {
+    char *original_dest = dest;
 
-    while (*dest)
-    {
+    while (*dest != '\0') {
         dest++;
     }
 
-    while (*src)
-    {
+    while (*src != '\0') {
         *dest = *src;
         dest++;
         src++;
     }
 
     *dest = '\0';
-    return start;
+
+    return original_dest;
 }
 
-char *_strncat(char *dest, char *src, int n)
-{
-    char *start = dest;
+char *_strncat(char *dest, char *src, int n) {
+    char *original_dest = dest;
 
-    while (*dest)
-    {
+    while (*dest != '\0') {
         dest++;
     }
 
-    while (*src && n > 0)
-    {
+    while (n > 0 && *src != '\0') {
         *dest = *src;
         dest++;
         src++;
@@ -129,130 +109,111 @@ char *_strncat(char *dest, char *src, int n)
     }
 
     *dest = '\0';
-    return start;
+
+    return original_dest;
 }
 
-char *_strncpy(char *dest, char *src, int n)
-{
-    char *start = dest;
+char *_strncpy(char *dest, char *src, int n) {
+    char *original_dest = dest;
 
-    while (*src && n > 0)
-    {
+    while (n > 0 && *src != '\0') {
         *dest = *src;
         dest++;
         src++;
         n--;
     }
 
-    while (n > 0)
-    {
+    while (n > 0) {
         *dest = '\0';
         dest++;
         n--;
     }
 
-    return start;
+    return original_dest;
 }
 
-int _strcmp(char *s1, char *s2)
-{
-    while (*s1 && *s2)
-    {
-        if (*s1 != *s2)
-        {
-            return *s1 - *s2;
-        }
+int _strcmp(char *s1, char *s2) {
+    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
         s1++;
         s2++;
     }
 
-    return *s1 - *s2;
+    return (*s1 - *s2);
 }
 
-char *_memset(char *s, char b, unsigned int n)
-{
-    char *start = s;
-    while (n > 0)
-    {
+char *_memset(char *s, char b, unsigned int n) {
+    char *original_s = s;
+
+    while (n > 0) {
         *s = b;
         s++;
         n--;
     }
-    return start;
+
+    return original_s;
 }
 
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-    char *start = dest;
-    while (n > 0)
-    {        *dest = *src;
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    char *original_dest = dest;
+
+    while (n > 0) {
+        *dest = *src;
         dest++;
         src++;
         n--;
     }
-    return start;
+
+    return original_dest;
 }
 
-char *_strchr(char *s, char c)
-{
-    while (*s)
-    {
-        if (*s == c)
-        {
+char *_strchr(char *s, char c) {
+    while (*s != '\0') {
+        if (*s == c) {
             return s;
         }
         s++;
     }
-    if (c == '\0')
-    {
-        return s;
-    }
+
     return NULL;
 }
 
-unsigned int _strspn(char *s, char *accept)
-{
+unsigned int _strspn(char *s, char *accept) {
     unsigned int count = 0;
-    while (*s && _strchr(accept, *s))
-    {
+
+    while (*s != '\0' && _strchr(accept, *s) != NULL) {
         count++;
         s++;
     }
+
     return count;
 }
 
-char *_strpbrk(char *s, char *accept)
-{
-    while (*s)
-    {
-        if (_strchr(accept, *s))
-        {
+char *_strpbrk(char *s, char *accept) {
+    while (*s != '\0') {
+        if (_strchr(accept, *s) != NULL) {
             return s;
         }
         s++;
     }
+
     return NULL;
 }
 
-char *_strstr(char *haystack, char *needle)
-{
-    while (*haystack)
-    {
-        char *start = haystack;
-        char *sub = needle;
+char *_strstr(char *haystack, char *needle) {
+    while (*haystack != '\0') {
+        char *h = haystack;
+        char *n = needle;
 
-        while (*haystack && *sub && *haystack == *sub)
-        {
-            haystack++;
-            sub++;
+        while (*h == *n && *n != '\0') {
+            h++;
+            n++;
         }
 
-        if (!*sub)
-        {
-            return start;
+        if (*n == '\0') {
+            return haystack;
         }
 
-        haystack = start + 1;
+        haystack++;
     }
 
     return NULL;
